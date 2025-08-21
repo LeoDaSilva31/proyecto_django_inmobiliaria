5869111e337c892d64d63d4a04f03d2805ec4f85
Leonardo Da Silva <ldasilva.personal@outlook.com>
2025-08-21 20:16:49 -0300

    seo: robots.txt servido por Django

--- django_inmobiliaria/urls.py
+++ django_inmobiliaria/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from propiedades.views import home, listado_propiedades, detalle_propiedad, buscar_propiedades, nosotros 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import HttpResponse
+
+
+def robots_txt(_request):
+    content = (
+        "User-agent: *\n"
+        "Allow: /\n"
+        "Sitemap: https://leods-blog.org/sitemap.xml\n"
+    )
+    return HttpResponse(content, content_type="text/plain")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,10 +27,11 @@ urlpatterns = [
     path("propiedades/<str:codigo>/", detalle_propiedad, name="propiedad_detalle"),
     path("buscar/", buscar_propiedades, name="buscar_propiedades"),
     path("nosotros/", nosotros, name="nosotros"),
+    path("robots.txt", robots_txt, name="robots_txt"),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
